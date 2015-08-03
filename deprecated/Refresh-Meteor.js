dcf24556f41e0dc77c7b4b3d8b15bf12d360aee1
Finnian Anderson <finnian@fxapi.co.uk>
2015-08-03 14:56:45 +0100

    Remove unnecessary code

--- deprecated/Refresh-Meteor.js
+++ deprecated/Refresh-Meteor.js
@@ -8,8 +8,6 @@ if (Meteor.isClient) {
   // This code only runs on the client
   console.log("Hey, client speaking!");
 
-//  Meteor.subscribe("t_1");
-//  Meteor.subscribe("t_2");
   Template.t_1.name = "Finnian";
   Template.t_2.name = "Alex";
 }
