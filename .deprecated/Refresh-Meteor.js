59610fd72b7b238cd139ccab2e578b5e0be5ea47
Popey Gilbert <popey@debenclipper.com>
2015-08-04 15:42:16 +0100

    Change some things

--- .deprecated/Refresh-Meteor.js
+++ .deprecated/Refresh-Meteor.js
@@ -1,13 +1,14 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isServer) {
-  console.log("Hey, server speaking!");
+	// This code only runs on the server
+    console.log("Hey, server speaking!");
 }
 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  console.log("Hey, client speaking!");
+    // This code only runs on the client
+    console.log("Hey, client speaking!");
 
-  Template.t_1.name = "Finnian";
-  Template.t_2.name = "Alex";
+    Template.t_1.name = "Finnian";
+    Template.t_2.name = "Alex";
 }
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Refresh-Meteor
 
 An application that can dynamically create high-quality questions on any topic.
+
+###### To Do List
+
+Create controller.js, a file which can change the template page.
+
+Create a series of pages with templates, so that controller.js can be tested.
+
+Transfer styling from Node to Meteor and create buttons which call controller.js in order to change the page.
+
+Start moving functionality from Node to Meteor
+
+Think about using their login page, it seems a large amount simpler.
